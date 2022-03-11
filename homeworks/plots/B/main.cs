ed887074ca068bf367751796e04e5a6fc2007125
Victor Rueskov Christiansen <vrueskov@gmail.com>
2022-03-11 16:16:00 +0100

    Corrected lngamma function

--- homeworks/plots/B/main.cs
+++ homeworks/plots/B/main.cs
@@ -19,8 +19,11 @@ public static class main{
 		if(x < 0) {
 			return Double.NaN;
 		}
-		double lngamma = x * Log(x + 1/(12*x - 1/x/10)) - x + Log(2*PI/x)/2;
-		return lngamma;
+		if(x < 9) {
+			return lngamma(x + 1) - Log(x);
+		}
+		double loggamma = x * Log(x + 1/(12*x - 1/x/10)) - x + Log(2*PI/x)/2;
+		return loggamma;
 	}
 
 	public static void Main(string[] args) {
